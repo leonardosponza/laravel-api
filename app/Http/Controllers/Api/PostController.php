f35abf58fde0e3b3276e529f53f7b29442299cda
leonardo <sponzal4@gmail.com>
2021-09-20 21:40:31 +0200

    fine

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Post;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json([
+            'success'=> true,
+            'results'=> $posts
+        ]);
+    }
+
+    
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+}
